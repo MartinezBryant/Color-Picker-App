49ac2760302e9fb0941d536c2478b24a577c1f45
Bryant Martinez <Bryantmartinez322@csu.fullerton.edu>
2024-10-27 16:41:33 -0700

    Initial Commit

---
+++ Color Picker AppTests/Color_Picker_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Color_Picker_AppTests.swift
+//  Color Picker AppTests
+//
+//  Created by Bryant Martinez on 10/27/24.
+//
+
+import Testing
+@testable import Color_Picker_App
+
+struct Color_Picker_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
